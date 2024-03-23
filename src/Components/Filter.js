72516cdd44ff1fa2e3151716f2f802dfdd73fa2a
Eblaraa <baraax01@gmail.com>
2024-03-24 00:14:40 +0700

    i added Filter box in left

---
+++ src/Components/Filter.js
@@ -0,0 +1,31 @@
+function Filter (){
+    return (
+        <>
+            <div className="filter" >
+                <h2>КОЛИЧЕСТВО ПЕРЕСАДОК</h2>
+                <div className="box" >
+                    <input type="checkbox" id="all" checked />
+                    <label for='all' >Все</label>
+                </div>
+                <div className="box" >
+                    <input type="checkbox" id="zero" checked />
+                    <label for='zero' >Без пересадок</label>
+                </div>
+                <div className="box" >
+                    <input type="checkbox" id="one" checked />
+                    <label for='one' >1 пересадка</label>
+                </div>
+                <div className="box" >
+                    <input type="checkbox" id="two" checked />
+                    <label for='two' >2 пересадка</label>
+                </div>
+                <div className="box" >
+                    <input type="checkbox" id="three" checked />
+                    <label for='three' >3 пересадка</label>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Filter;
