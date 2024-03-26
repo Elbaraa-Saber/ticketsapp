35ebfe781d0fd9c5c5d0300481db8c40edb81ab6
Eblaraa <baraax01@gmail.com>
2024-03-26 10:41:44 +0700

    i added onclick func in inputs to start do filter

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,28 +1,69 @@
 import '../Css/filter.css'
+import { useState } from 'react';
 
 function Filter (){
+    const [isChecked, setIsChecked] = useState({
+        all: false,
+        zero: false,
+        one: false,
+        two: false,
+        three: false
+    });
+
+    const handleCheckboxClick = (id) => {
+        setIsChecked(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
+
     return (
         <>
             <div className="filter" >
                 <h2>КОЛИЧЕСТВО ПЕРЕСАДОК</h2>
                 <div className="box" >
-                    <input type="checkbox" className='checked' id="all" />
+                    <input
+                        type="checkbox"
+                        className={isChecked.all ? 'clicked' : ''}
+                        id="all"
+                        onClick={() => handleCheckboxClick('all')}
+                    />
                     <label htmlFor="all" >Все</label>
                 </div>
                 <div className="box" >
-                    <input type="checkbox" className='checked' id="zero"  />
+                    <input
+                        type="checkbox"
+                        className={isChecked.zero ? 'clicked' : ''}
+                        id="zero"
+                        onClick={() => handleCheckboxClick('zero')}
+                    />
                     <label htmlFor="zero" >Без пересадок</label>
                 </div>
                 <div className="box" >
-                    <input type="checkbox" id="one" />
+                    <input
+                        type="checkbox"
+                        className={isChecked.one ? 'clicked' : ''}
+                        id="one"
+                        onClick={() => handleCheckboxClick('one')}
+                    />
                     <label htmlFor="one" >1 пересадка</label>
                 </div>
                 <div className="box" >
-                    <input type="checkbox" id="two"  />
+                    <input
+                        type="checkbox"
+                        className={isChecked.two ? 'clicked' : ''}
+                        id="two"
+                        onClick={() => handleCheckboxClick('two')}
+                    />
                     <label htmlFor="two" >2 пересадка</label>
                 </div>
                 <div className="box" >
-                    <input type="checkbox" id="three"  />
+                    <input
+                        type="checkbox"
+                        className={isChecked.three ? 'clicked' : ''}
+                        id="three"
+                        onClick={() => handleCheckboxClick('three')}
+                    />
                     <label htmlFor="three" >3 пересадка</label>
                 </div>
             </div>
